a1345f50c62a0b54c661aeacc4ad52454064bda4
SonCrits <truongson290893@gmail.com>
2023-03-03 06:55:45 +0700

    [WIP][IMP] OWL: add delete and edit function

--- OWL/ToDo APP/app.js
+++ OWL/ToDo APP/app.js
@@ -3,19 +3,53 @@ const { Component, mount, xml, useState} = owl;
 class Task extends Component {
     static template = xml`
         <li t-attf-style="background-color:#{props.task.color}" class="d-flex align-items-center justify-content-between border p-3 mb-2 rounded">
-            <div class="form-check form-switch fs-5">
-                <input class="form-check-input" type="checkbox" value="" t-att-id="props.task.id"/>
-                <label class="form-check-label" for="flexCheckDefault" t-att-for="props.task.id">
+            <div t-if="state.isEditing" class="d-flex align-items-center flex-grow-1 me-2">
+                <input type="text" class="form-control me-2" t-model="state.name"/>
+                <input type="color" style="width: 60px;" class="form-control-lg form-control-color border-0 bg-white" 
+                    id="color" title="Choose your color" t-model="state.color"/>
+            </div>
+            <div t-if="!state.isEditing" class="form-check form-switch fs-5">
+                <input class="form-check-input" type="checkbox" value="" t-att-id="props.task.id" t-on-click="toggleTask"/>
+                <label class="form-check-label" for="flexCheckDefault" t-att-for="props.task.id"
+                    t-attf-class="#{props.task.isCompleted ? 'text-decoration-line-through': ''}">
                     <t t-esc="props.task.name"/>
                 </label>
             </div>
             <div>
-                <button class="btn btn-primary me-2"><i class="bi bi-pencil"></i></button>
-                <button class="btn btn-danger"><i class="bi bi-trash"></i></button>
+                <button t-if="!state.isEditing" class="btn btn-primary me-2" t-on-click="editTask"><i class="bi bi-pencil"></i></button>
+                <button t-if="state.isEditing" class="btn btn-primary me-2" t-on-click="saveTask"><i class="bi bi-check-lg"></i></button>
+                <button class="btn btn-danger" t-on-click="deleleTask"><i class="bi bi-trash"></i></button>
             </div>
         </li>
     `
-    static props = ["task"]
+    static props = ["task", "onDelete"]
+
+    setup() {
+        this.state = useState({
+            isEditing: false,
+            id: this.props.task.id,
+            name: this.props.task.name,
+            color: this.props.task.color,
+            isCompleted: this.props.task.isCompleted
+        })
+    }
+
+    toggleTask() {
+        this.props.task.isCompleted = !this.props.task.isCompleted
+    }
+
+    deleleTask() {
+        this.props.onDelete(this.props.task)
+    }
+
+    editTask() {
+        this.state.isEditing = true
+    }
+
+    saveTask() {
+        this.state.isEditing = false
+        console.log(this.state)
+    }
 }
 
 class Root extends Component {
@@ -34,7 +68,7 @@ class Root extends Component {
 
         <ul class="d-flex flex-column mt-5 p-0">
             <t t-foreach="tasks" t-as="task" t-key="task.id">
-                <Task task="task"/>
+                <Task task="task" onDelete.bind="deleteTask"/>
             </t>
         </ul>
     `
@@ -65,6 +99,11 @@ class Root extends Component {
     
     let state = this.state;
     this.state = {...state, name: "", color: "#FFF000"}
+    }
+
+    deleteTask(task) {
+        const index = this.tasks.findIndex(t=>t.id == task.id)
+        this.tasks.splice(index, 1)
     }
 
     
