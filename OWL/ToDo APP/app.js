9890a840f8b15b44bfc59635dfc0d03f953a36f0
SonCrits <truongson290893@gmail.com>
2023-03-10 21:19:51 +0700

    [IMP][FIX] owl: add some one UI for task component

--- OWL/ToDo APP/app.js
+++ OWL/ToDo APP/app.js
@@ -2,8 +2,14 @@ const { Component, xml, useState, mount } = owl;
 
 class Task extends Component {
     static template = xml`
-        <li t-attf-style="background-color: #{props.task.color}" class="d-flex align-items-center justify-content-between border p-3 rounded mb-2">
-            <div class="form-check form-switch fs-5">
+        <li t-attf-style="background-color: #{props.task.color}" class="d-flex align-items-center 
+            justify-content-between border p-3 rounded mb-2">
+            <div class="d-flex align-items-center flex-grow-1 me-2">
+                <input type="text" class="form-control me-2"/>
+                <input type="color" style="width: 60px" class="form-control-lg form-control-color border-0 bg-white m-0" 
+                    id="color" title="Choose your color"/>
+            </div>
+            <div class="form-check form-switch fs-5 d-none">
                 <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" 
                     t-att-id="props.task.id" t-att-checked="props.task.isCompleted" t-on-click="toggleTask"/>
                 <label class="form-check-label" t-att-for="props.task.id" t-attf-class="#{props.task.isCompleted ?
@@ -36,7 +42,7 @@ class Root extends Component {
         <div class="input-group-lg d-flex w-100 rounded border align-items-center">
             <input type="text" class="form-control-lg flex-fill border-0 me-1" placeholder="Add your new task" 
             aria-label="Recipient's username" aria-describedby="button-addon2" t-model="state.name" t-att-value="state.name"/>
-            <input type="color" class="form-control-lg form-control-color border-0 bg-white" id="color" t-att-value="state.color" 
+            <input type="color" class="form-control-lg form-control-color border-0 bg-white m-0" id="color" t-att-value="state.color" 
                 title="Choose your color" t-model="state.color"/>
             <button class="btn btn-primary" type="button" id="button-addon2" t-on-click="addTask">
                 <i class="bi bi-plus-lg fs-3"></i>
