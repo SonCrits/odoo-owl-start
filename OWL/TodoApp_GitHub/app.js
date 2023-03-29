48c6754d1efb0fae8e99b81f6418141d413e3e57
SonCrits <truongson290893@gmail.com>
2023-03-29 13:42:03 +0700

    [IMP] todo_app_github: Displaying a list of Task - Create a list of tasks, with each task including `id`, `text`, `isCompleted` status - Use t-foreach to iterate through task list just created - Use t-as to assign each element in the task list a value - Use t-key to mark records by id - Use t-att-checked makes it dynamic

--- OWL/TodoApp_GitHub/app.js
+++ OWL/TodoApp_GitHub/app.js
@@ -2,7 +2,29 @@ const { Component, xml, mount} = owl;
 
 // Owl Components
 class Root extends Component {
-    static template = xml`<div>Todo App</div>`;
+    static template = xml/* xml */`
+        <div class="task-list">
+            <t t-foreach="tasks" t-as="task" t-key="task.id">
+                <div class="task">
+                    <input type="checkbox" t-att-checked="task.isCompleted"/>
+                    <span><t t-esc="task.text"/></span>
+                </div>
+            </t>
+        </div>
+    `;
+
+    tasks = [
+        {
+            id: 1,
+            text: "Learning Owl",
+            isCompleted: true
+        },
+        {
+            id: 2,
+            text: "Learning Data structures and Algorithms",
+            isCompleted: true
+        }
+    ]
 } 
 
 mount(Root, document.body);
