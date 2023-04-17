5fe8edce35c5a27d4d980c141a5f25872274421c
SonCrits <truongson290893@gmail.com>
2023-04-17 10:44:08 +0700

    [IMP] todo_app_github: Using store

--- OWL/TodoApp_GitHub/app.js
+++ OWL/TodoApp_GitHub/app.js
@@ -1,82 +1,98 @@
-const { Component, xml, mount, useRef, onMounted, useState} = owl;
+const { Component, mount, xml, useRef, onMounted, useState, reactive, useEnv } = owl;
 
-// ==================================================
-// Task Component
-// ==================================================
-
-class Task extends Component {
-    static template = xml /* xml */`
-        <div class="task" t-att-class="props.task.isCompleted ? 'done' : '' ">
-            <input type="checkbox" t-att-checked="props.task.isCompleted" t-on-click="toggleTask"/>
-            <span><t t-esc="props.task.text"/></span>
-            <span class="delete" t-on-click="deleteTask">🗑</span>
-        </div>
-    `;
-
-    static props = ["task", "onDelete"];
-
-    deleteTask() {
-        this.props.onDelete(this.props.task);
-    }
-
-    toggleTask() {
-        this.props.task.isCompleted = !this.props.task.isCompleted;
-    }
+// -------------------------------------------------------------------------
+// Store
+// -------------------------------------------------------------------------
+function useStore() {
+  const env = useEnv();
+  return useState(env.store);
 }
-// ==================================================
-// Root Components
-// ==================================================
+
+// -------------------------------------------------------------------------
+// TaskList
+// -------------------------------------------------------------------------
+class TaskList {
+  nextId = 1;
+  tasks = [];
+
+  addTask(text) {
+    text = text.trim();
+    if (text) {
+      const task = {
+        id: this.nextId++,
+        text: text,
+        isCompleted: false,
+      };
+      this.tasks.push(task);
+    }
+  }
+
+  toggleTask(task) {
+    task.isCompleted = !task.isCompleted;
+  }
+
+  deleteTask(task) {
+    const index = this.tasks.findIndex((t) => t.id === task.id);
+    this.tasks.splice(index, 1);
+  }
+}
+
+function createTaskStore() {
+  return reactive(new TaskList());
+}
+
+// -------------------------------------------------------------------------
+// Task Component
+// -------------------------------------------------------------------------
+class Task extends Component {
+  static template = xml/* xml */ `
+    <div class="task" t-att-class="props.task.isCompleted ? 'done' : ''">
+      <input type="checkbox" t-att-checked="props.task.isCompleted" t-on-click="() => store.toggleTask(props.task)"/>
+      <span><t t-esc="props.task.text"/></span>
+      <span class="delete" t-on-click="() => store.deleteTask(props.task)">🗑</span>
+    </div>`;
+
+  static props = ["task"];
+
+  setup() {
+    this.store = useStore();
+  }
+}
+
+// -------------------------------------------------------------------------
+// Root Component
+// -------------------------------------------------------------------------
 class Root extends Component {
-    static template = xml /* xml */`
-        <div class="todo-app">
-            <input placeholder="Enter a new task" t-on-keyup="addTask" t-ref="add-input"/>
-            <div class="task-list">
-                <t t-foreach="tasks" t-as="task" t-key="task.id">
-                    <Task task="task" onDelete.bind="deleteTask"/>
-                </t>
-            </div>
-        </div>
-    `;
+  static template = xml/* xml */ `
+    <div class="todo-app">
+      <input placeholder="Enter a new task" t-on-keyup="addTask" t-ref="add-input"/>
+      <div class="task-list">
+        <t t-foreach="store.tasks" t-as="task" t-key="task.id">
+          <Task task="task"/>
+        </t>
+      </div>
+    </div>`;
+  static components = { Task };
 
-    static components = {Task};
+  setup() {
+    const inputRef = useRef("add-input");
+    onMounted(() => inputRef.el.focus());
+    this.store = useStore();
+  }
 
-    // Khi nhập tên task sau đó enter từ `input`, luồng hoạt động trong hàm `addTask` vẫn thực hiện
-    // Tuy nhiên do owl không hiểu cần kết xuất dữ liệu cho người dùng (interface)
-    // Sử dụng `useState` làm cho các tác vụ hoạt động trở lại
-    nextId = 1;
-    tasks = useState([]);
-
-    addTask(ev) {
-        // 13 is keycode for Enter
-        if (ev.keyCode === 13) {
-            const text = ev.target.value.trim();
-            ev.target.value = "";
-            if (text){
-                const newTask = {
-                    id: this.nextId++,
-                    text: text,
-                    isCompleted: false,
-                };
-                this.tasks.push(newTask);
-            }       
-        }
+  addTask(ev) {
+    // 13 is keycode for ENTER
+    if (ev.keyCode === 13) {
+      this.store.addTask(ev.target.value);
+      ev.target.value = "";
     }
+  }
+}
 
-    deleteTask(task) {
-        const index = this.tasks.findIndex(t => t.id === task.id);
-        this.tasks.splice(index, 1)
-    }
-
-    // khi trang load, Root components được sẵn sàng sẽ gọi onMounted(), sau đó sẽ chỉ con trỏ focus tới inputRef
-    // useRef dùng để tham chiếu tới phần tử html có thuộc tính t-ref = add-input
-    setup() {
-        const inputRef = useRef("add-input");
-        onMounted(() => inputRef.el.focus());
-    }
-} 
-
-// ====================================================
+// -------------------------------------------------------------------------
 // Setup
-// ====================================================
-
-mount(Root, document.body, {dev: true});
+// -------------------------------------------------------------------------
+const env = {
+  store: createTaskStore(),
+};
+mount(Root, document.body, { dev: true, env });
