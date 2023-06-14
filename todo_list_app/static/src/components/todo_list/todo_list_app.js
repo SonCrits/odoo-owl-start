7fa284496e6ac6ec93f8e793a6dfe6df8df6a4e7
Tran Truong Son <truongson290893@gmail.com>
2023-06-14 09:38:35 +0700

    [IMP] todo_list_app: add static todo list to components

--- todo_list_app/static/src/components/todo_list/todo_list_app.js
+++ todo_list_app/static/src/components/todo_list/todo_list_app.js
@@ -5,7 +5,13 @@ const { Component, useState } = owl;
 
 export class TodoListApp extends Component {
     setup() {
-        this.state = useState({value: 1})
+        this.state = useState({
+            taskList: [
+                {id: 1, name: "Task 1"},
+                {id: 2, name: "Task 2"},
+                {id: 3, name: "Task 3"},
+            ]
+        })
     }
 
 }
