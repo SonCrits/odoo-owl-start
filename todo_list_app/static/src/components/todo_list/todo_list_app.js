12c0bffa1ece14c75601ab5243401e74743acfca
Tran Truong Son <truongson290893@gmail.com>
2023-06-14 09:53:25 +0700

    [IMP] todo_list_app: add static completed state for task components

--- todo_list_app/static/src/components/todo_list/todo_list_app.js
+++ todo_list_app/static/src/components/todo_list/todo_list_app.js
@@ -7,9 +7,9 @@ export class TodoListApp extends Component {
     setup() {
         this.state = useState({
             taskList: [
-                {id: 1, name: "Task 1", color: "#FF0000"},
-                {id: 2, name: "Task 2", color: "#000000"},
-                {id: 3, name: "Task 3", color: "#FFFFFF"},
+                {id: 1, name: "Task 1", color: "#FF0000", completed: true},
+                {id: 2, name: "Task 2", color: "#000000", completed: true},
+                {id: 3, name: "Task 3", color: "#FFFFFF", completed: false},
             ]
         })
     }
