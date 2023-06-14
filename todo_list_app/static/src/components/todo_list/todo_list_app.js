857adb922940e5c82dd57658025c8a33df265381
Tran Truong Son <truongson290893@gmail.com>
2023-06-14 10:46:02 +0700

    [IMP] todo_list_app: use data for task components - use data is called by backend instead of static todo list - change the column name of `is_completed` field instead of `completed` to math the template

--- todo_list_app/static/src/components/todo_list/todo_list_app.js
+++ todo_list_app/static/src/components/todo_list/todo_list_app.js
@@ -1,17 +1,25 @@
 /** @odoo-module **/
-
+import { useService } from "@web/core/utils/hooks";
 import { registry } from '@web/core/registry';
-const { Component, useState } = owl;
+const { Component, useState, onWillStart } = owl;
 
 export class TodoListApp extends Component {
     setup() {
         this.state = useState({
-            taskList: [
-                {id: 1, name: "Task 1", color: "#FF0000", completed: true},
-                {id: 2, name: "Task 2", color: "#000000", completed: false},
-                {id: 3, name: "Task 3", color: "#FFFFFF", completed: false},
-            ]
+            taskList: []
         })
+
+        this.orm = useService("orm")
+        this.model = "todo.list.app"
+
+        onWillStart(async ()=> {
+            await this.getAllTask()
+        })
+
+    }
+
+    async getAllTask() {
+        this.state.taskList = await this.orm.call(this.model, 'search_read', [[], ["name", "color", "completed"]],)
     }
 
 }
